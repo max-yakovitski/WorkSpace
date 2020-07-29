050a14d8a6df70fc6f39c68c9da1b701ce61b70d
max-yakovitski <m.yakovitski@gmail.com>
2020-07-30 01:26:36 +0300

    new ATM + Circle + Foursquare

--- MyTasks/src/lesson3/ATM.java
+++ MyTasks/src/lesson3/ATM.java
@@ -2,57 +2,88 @@ package lesson3;
 
 public class ATM {
 
-	private int banknoteQuantity;
-	private int banknote1;
-	private int banknote2;
-	private int banknote3;
-	boolean check = true;
+	private int banknoteQty20 = 15;
+	private int banknote20 = 20;
+	private int banknoteQty50 = 10;
+	private int banknote50 = 50;
+	private int banknoteQty100 = 5;
+	private int banknote100 = 100;
+	private int total = banknoteQty20 + banknoteQty50 + banknoteQty100;
 	
-	public ATM(int banknoteQuantity, int banknote1, int banknote2, int banknote3) {
-		this.banknoteQuantity = banknoteQuantity;
-		this.banknote1 = banknote1;
-		this.banknote2 = banknote2;
-		this.banknote3 = banknote3;
+	public ATM() {
+		
+	}
+	
+	public ATM(int banknoteQty20, int banknoteQty50, int banknoteQty100) {
+		this.banknoteQty20 = banknoteQty20;
+		this.banknoteQty50 = banknoteQty50;
+		this.banknoteQty100 = banknoteQty100;
 	}
 
 	public void getInfoATM() {
-		System.out.println("Купюр в банкомате: " + banknoteQuantity);
-		System.out.println("Номиналы купюр: " + banknote1 + ", " + banknote2 + ", " + banknote3);
+		System.out.println("Купюр в банкомате: " + total);
+		System.out.println("Номиналы купюр: " + banknote20 + " рублей  - " + banknoteQty20 + " штук" );
+		System.out.println("Номиналы купюр: " + banknote50 + " рублей - " + banknoteQty50 + " штук");
+		System.out.println("Номиналы купюр: " + banknote100 + " рублей - " + banknoteQty100 + " штук");
 		System.out.println();
 	}
 	
-	public void addMoneytoATM(int banknoteQuantity, int banknote) {
-		if (banknote == this.banknote1 || banknote == this.banknote2 || banknote == this.banknote3) {
-			this.banknoteQuantity += banknoteQuantity;
-			System.out.println("В банкомате стало " + this.banknoteQuantity + " купюр, было внесено " + banknoteQuantity + " купюр c номиналом " + banknote);
+	public void addMoneytoATM(int quantity, int banknote) {
+		if (banknote == this.banknote20) {
+			this.banknoteQty20 += quantity;
+			total+=quantity;
+			System.out.println("В банкомат было внесено: " + quantity + " банкнот(а) с номиналом - " +  banknote +" рублей");
+		}
+		else if (banknote == this.banknote50) {
+			this.banknoteQty50 += quantity;
+			total+=quantity;
+			System.out.println("В банкомат было внесено: " + quantity + " банкнот(а) с номиналом - " +  banknote +" рублей");
+		}
+		else if (banknote == this.banknote100) {
+			this.banknoteQty100 += quantity;
+			total+=quantity;
+			System.out.println("В банкомат было внесено: " + quantity + " банкнот(а) с номиналом - " +  banknote +" рублей");
 		}
 		else {
 			System.out.println("Внимание! Эти банкноты не могут быть загружены в банкомат.");
 		}
+		System.out.println();
 	}
 	
-	public void getMoneyfromATM(int banknoteQuantity, int banknote, boolean check) {
-		if (check == true) {
-			if (banknote == this.banknote1 || banknote == this.banknote2 || banknote == this.banknote3) {
-				this.banknoteQuantity -= banknoteQuantity;
-				System.out.println("В банкомате стало " + this.banknoteQuantity + " купюр, было снято " + banknoteQuantity + " купюр c номиналом " + banknote);
+	public void getMoneyfromATM(int quantity, int banknote, boolean check) {
+		if (check == true && quantity <= banknoteQty20 && quantity <= banknoteQty50 && quantity <= banknoteQty100) {
+			if (banknote == this.banknote20) {
+				this.banknoteQty20 -= quantity;
+				total-=quantity;
+				System.out.println("В банкомате сняли: " + quantity + " банкнот с номиналом - " +  banknote +" рублей");
 			}
-			else {
-				System.out.println("Внимание! Таких банкнот -  " + banknote + ", нет в банкомате!");
+			if (banknote == this.banknote50) {
+				this.banknoteQty50 -= quantity;
+				total-=quantity;
+				System.out.println("В банкомате сняли: " + quantity + " банкнот с номиналом - " +  banknote +" рублей");
 			}
-			
+			if (banknote == this.banknote100) {
+				this.banknoteQty100 -= quantity;
+				total-=quantity;
+				System.out.println("В банкомате сняли: " + quantity + " банкнот с номиналом - " +  banknote +" рублей");
+			}
 		}
 		else {
-			System.out.println("Сбой в работе банкомата, cнятие " + banknoteQuantity + " купюр номиналом " + banknote + " не удалось!");
+			System.out.println("Сбой в работе банкомата, cнятие " + quantity + " купюр номиналом " + banknote + " не удалось! Недостаточно нужных купюр!");
 		}
-		
+		System.out.println();
 	}
 	
 	public static void main(String[] args) {
-		ATM atm24 = new ATM(100, 20, 50, 100);
+		ATM atm24 = new ATM();
 		atm24.getInfoATM();
-		atm24.addMoneytoATM(100, 20);
-		atm24.getMoneyfromATM(5, 50, true); 
+		atm24.addMoneytoATM(1, 100);
+		atm24.getMoneyfromATM(5, 50, true);
+		atm24.getInfoATM();
+		atm24.getMoneyfromATM(6, 50, true); 
+		atm24.getInfoATM();
+		atm24.addMoneytoATM(3, 50);
+		atm24.getInfoATM();
 
 
 	}
---
+++ MyTasks/src/lesson3/Figure.java
@@ -0,0 +1,7 @@
+package lesson3;
+
+public interface Figure {
+	
+	public void getSquare();
+		
+}
